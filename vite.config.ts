9b8492ec4621fa9841d8c07ab87d118cf7517d4b
Cory House <housecor@gmail.com>
2025-04-05 11:06:08 -0500

    a

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
-/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    include: ["src/**/*.{test,spec}.{js,ts,jsx,tsx}"],
-    environment: "happy-dom",
-    setupFiles: ["./test-setup.js"],
-    globals: true,
-  },
 });

f606dc835c171a597c6fa187742e43272f8afd69
Cory House <housecor@gmail.com>
2025-04-23 09:51:50 -0500

    a

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Contact from "./Contact.tsx";
+import Hello from "./Hello.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Hello />
     <Contact />
   </StrictMode>
 );
